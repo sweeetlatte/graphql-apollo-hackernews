b3a621cccc076f7a2f5f7d4c518347e41ba09d73
sweeetlatte <thuylinh271055@gmail.com>
2022-12-06 16:37:34 +0700

    typeDefs

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,22 +2,51 @@ const { ApolloServer } = require("apollo-server");
 
 /**
  * The typeDefs constant defines your GraphQL schema.
- * Here is a simple Query type with one field called info. This field has the type String!.
- * The exclamation mark in the type definition means that this field is required and can never be null.
  */
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`;
+
+let links = [
+  {
+    id: "link-0",
+    url: "https://www.allrecipes.com/recipe/266826/air-fryer-potato-wedges/",
+    description: "Air Fryer Potato Wedges",
+  },
+];
 
 /**
  * The resolvers object is the actual implementation of the GraphQL schema.
  * Notice how its structure is identical to the structure of the type definition inside typeDefs: Query.info.
  */
+
+/**
+ * Every GraphQL resolver function actually receives four input arguments.
+ * The first argument, commonly called parent (or sometimes root) is the result of the previous resolver execution level.
+ *
+ * The query:
+ * query {
+ *  feed {
+ *      id
+ *      url
+ *      description
+ *  }
+ * }
+ * has two of these execution levels.
+ *
+ * On the first level, it invokes the feed resolver and returns the entire data stored in links.
+ * For the second execution level, the GraphQL server invokes the resolvers of the Link type
+ * (because thanks to the schema, it knows that feed returns a list of Link elements)
+ * for each element inside the list that was returned on the previous resolver level.
+ *
+ * Therefore, in all of the three Link resolvers, the incoming parent object is the element inside the links list.
+ */
 const resolvers = {
   Query: {
-    info: () => null,
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   },
 };
 
@@ -26,7 +55,7 @@ const resolvers = {
  * This tells the server what API operations are accepted and how they should be resolved.
  */
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
 });
 
