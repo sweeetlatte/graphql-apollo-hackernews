522f19991ae1040223b215ef37f6f26d237cad73
sweeetlatte <thuylinh271055@gmail.com>
2022-12-09 15:04:42 +0700

    filter links

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import CreateLink from "./CreateLink";
 import Header from "./Header";
 import Login from "./Login";
+import Search from "./Search";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<LinkList />} />
           <Route path="/create" element={<CreateLink />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/search"element={<Search/>}/>
         </Routes>
       </div>
     </div>
